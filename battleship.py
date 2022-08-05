b2ede07b22d8e7d81fad4236825b3e53ea0b0e1b
sohsht <sohsht@uw.edu>
2022-08-05 11:58:13 -0700

    added logic for starting game

--- battleship.py
+++ battleship.py
@@ -1,3 +1,48 @@
 # Code Academy CS101 Final Project
 # BattleShip game inside of terminal using python
 
+from battleship_board import Board
+import sys
+
+class BattleShip:
+    
+    def __init__(self):
+        self.name = ""
+        self.size = 0
+        
+        print("Enter your name:")
+        self.name = input()
+        print("Hello {}, welcome to Battle Ship".format(self.name))
+        print("Enter the size of the board (8, 9, 10)")
+        self.size = int(input())
+        print("\n \n \n")
+        self.board = Board(self.name, self.size)
+
+    
+def game_start():
+    new_game = 0
+    print("Battle Ship")
+    print("Do you want to start a new game? Y/N")
+    ans = input()
+    if ans.upper() == "Y":
+        new_game = BattleShip()
+    else:
+        print("Ending Game")
+        new_game = 0
+    return new_game
+
+def show_game(player, pc):
+    print(player_board)
+    print(pc_board)
+
+    
+# game start
+new_game = game_start()
+if new_game == 0:
+    sys.exit()
+player_board = new_game.board
+pc_board = Board("PC", new_game.size)
+
+show_game(player_board, pc_board)
+
+
--- battleship_board.py
+++ battleship_board.py
@@ -1,19 +1,22 @@
 # Board Class
-from ssl import AlertDescription
-
-
 class Board:
+    ships = [["Aircraft Carrier", 5], ["Battleship", 4], ["Cruiser", 3], ["Destroyer", 2], ["Submarine", 1]]
 
     def __init__(self, name, size=8):
         self.size = size
         self.name = name
+        self.alive = True
         self.board = []
         self.letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        if self.name is not "PC":
+            self.user = True
+        else:
+            self.user = False
         for i in range(size ** 2):
             self.board.append([i, "[ ]"])
     
     def __repr__(self):
-        final = ""
+        final = ("{}\n".format(self.name))
         num_line = "   "
         for i in range(self.size):
             num_line += " {} ".format(i + 1)
@@ -29,12 +32,31 @@ class Board:
             else:
                 continue
         return final
+
+    def has_boat(self, square):
+        # checks to see if the square has a boat
+        if self.board[square] == "[#]":
+            return True
+        else:
+            return False
     
+    def auto_populate(self):
+        return 0
 
-test_board = Board("test", 10)
-print(test_board)
+    def is_alive(self):
+        return self.alive
+    
+    def shot_fired(self, x, alpha):
+        square = 0
+        y = self.letters.index(alpha)
+        square = y * self.size + x
 
-
+        if self.has_boat(square):
+            print("{} has been hit at coordinate {}, {}".format(self.name, x, alpha))
+        else:
+            print("Shot was fired at coordinate {}, {}. There was no ship there.".format(x, alpha))
+    
+        
             
             
 
