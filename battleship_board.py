a3624da6ec1df9578d773bec8d17c1d0810383ef
sohsht <sohsht@uw.edu>
2022-08-04 23:44:53 -0700

    prints the board cleanly

--- battleship_board.py
+++ battleship_board.py
@@ -6,19 +6,22 @@ class Board:
 
     def __init__(self, name, size=8):
         self.size = size
-        self.board = []
         self.name = name
+        self.board = []
+        self.letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
         for i in range(size ** 2):
             self.board.append([i, "[ ]"])
     
     def __repr__(self):
         final = ""
-        num_line = ""
+        num_line = "   "
         for i in range(self.size):
             num_line += " {} ".format(i + 1)
         final += num_line + "\n"
         line = ""
         for i in range(self.size ** 2):
+            if i in range(0, self.size ** 2, self.size):
+                line += " {} ".format(self.letters[round(i / self.size)])
             line += self.board[i][1]
             if (i + 1) % self.size == 0:
                 final += line + "\n"
