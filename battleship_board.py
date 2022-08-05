ae07d4e6f6319c17680a5841016047459adae23c
sohsht <sohsht@uw.edu>
2022-08-04 23:22:17 -0700

    created the repr for the board class.

--- battleship_board.py
+++ battleship_board.py
@@ -1,5 +1,42 @@
 # Board Class
+from ssl import AlertDescription
+
+
 class Board:
-    def __init__(self, size=8):
-        pass
+
+    def __init__(self, name, size=8):
+        self.size = size
+        self.board = []
+        self.name = name
+        for i in range(size ** 2):
+            self.board.append([i, "[ ]"])
+    
+    def __repr__(self):
+        final = ""
+        num_line = ""
+        for i in range(self.size):
+            num_line += " {} ".format(i + 1)
+        final += num_line + "\n"
+        line = ""
+        for i in range(self.size ** 2):
+            line += self.board[i][1]
+            if (i + 1) % self.size == 0:
+                final += line + "\n"
+                line = ""
+            else:
+                continue
+        return final
+    
+
+test_board = Board("test", 10)
+print(test_board)
+
+
+            
+            
+
+
+
+
+        
 
